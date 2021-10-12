7b52c79f8cea2db7b0629e3305a20b9736469d1e
Sharon <sharonelizalex@gmail.com>
2021-10-13 00:19:21 +1100

    initial psuedo code

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('hey')
+/**
+ * 1. needs user input. use js prompt
+ * 2.capture that input
+ * 3. Have an array with [r, p, s]
+ * 4. randomly generate a number 0 to 1 and select one index
+ * 5. alert saying computer played this.
+ * 6. compare the result and alert if win or lose
+ * 7. based on who win , the stats need to be stored and displayed
+ * 8. ask if player wants to play again
+ * 9. if yes then ask the prompt again
+ * 10. this will continue in a loop.
+ * 11. Store this in the stats variable.
+ * 12. display the stats.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
